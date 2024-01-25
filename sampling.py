2a311f250b88f9a8bcbce03850359b6bebbbdfb2
ʚ🐇ɞ <oncwnuBxE1h4jlZZBD84DMebLRtI@git.weixin.qq.com>
2024-01-26 00:20:24 +0800

    q_learning_sampling

--- sampling.py
+++ sampling.py
@@ -5,45 +5,53 @@ import os
 from PIL import Image
 import numpy as np
 
-from model import QLearning
+from model import QLearning, ActorCritic
 
 epochs = 10000
 load_path="./ckpts/v2.pth"
 MAX_ITERATION = 10000
-render = True
-required = 100
+render = False
+required = 500
 env = gym.make('LunarLander-v2',
                 render_mode = "rgb_array")
 
-policy = ActorCritic()
-# policy.load_state_dict(torch.load(load_path))
+policy = QLearning()
+policy.load_state_dict(torch.load(load_path))
 
 records = []
 count = 0
-for epoch in range(1, epochs+1):
-    obser, _ = env.reset()
-    running_reward = 0
-    record = []
-    for t in range(1, MAX_ITERATION+1):
-        action = policy(obser)
-        r = list(obser)
-        r.append(action)
-        record.append(r)
-        obser, reward, terminated, truncated, _ = env.step(action)
-        running_reward += reward
-        if render and epoch%100==0:
-            img = env.render()
-            img = Image.fromarray(img)
-            gif_dir = "./gifs/pretrained/{:02d}".format(epoch)
-            if not os.path.exists(gif_dir):
-                os.makedirs(gif_dir)
-            img.save(os.path.join(gif_dir, "{:04d}.png".format(t)))
-            print (obser,action,reward)
-        if terminated or truncated:
-            break
+with torch.no_grad():
+    for epoch in range(1, epochs+1):
+        obser, _ = env.reset()
+        running_reward = 0
+        record = []
+        for t in range(1, MAX_ITERATION+1):
+            out = policy(obser)
+            action_probs = F.softmax(out, dim=0)
+            action = int(torch.argmax(action_probs, dim=0))
+            r = list(obser)
+            r.append(action)
+            record.append(r)
+            obser, reward, terminated, truncated, _ = env.step(action)
+            running_reward += reward
+            if render:
+                img = env.render()
+                img = Image.fromarray(img)
+                gif_dir = "./gifs/test/{:02d}".format(epoch)
+                if not os.path.exists(gif_dir):
+                    os.makedirs(gif_dir)
+                img.save(os.path.join(gif_dir, "{:04d}.png").format(t))
 
-        # recored_actions.append(actions)
-    print('Episode {}\tReward: {}'.format(epoch, running_reward))
+            if terminated or truncated:
+                break
+
+        if running_reward >= 200:
+            count += 1
+            for r in record:
+                records.append(r)
+
+        if count == required:
+            break
 
 env.close()
 
